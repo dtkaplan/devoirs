7231c7e884191a6f2865cf35776b6d4764c12ea9
Daniel Kaplan <dtkaplan@gmail.com>
2024-10-29 07:52:25 -0500

    working on summarize_student_doc()

--- R/grading.R
+++ R/grading.R
@@ -31,7 +31,7 @@ get_course_params <- function(home = ".") {
 get_new_submissions <- function(home = ".") {
   params <- get_course_params(home)
   tmp <- googlesheets4::read_sheet(params$`submissions-file`)
-  docids <- get_doc_id(tmp)
+  docids <- get_doc_id_helper(tmp)
   if (nrow(tmp) == 0) warning("No new submissions")
 
   tmp <- tmp |> dplyr::mutate(docid = docids)
@@ -45,7 +45,7 @@ get_new_submissions <- function(home = ".") {
 #' @rdname grading
 #' @export
 get_historic_data <- function(home = ".", since = "2000-1-1 00:00:01 UTC") {
-  since <- convert_time(since)
+  since <- convert_time_helper(since)
   if (file.access("Permanent_store.RDS") == 0) {
     tmp <- readRDS("Permanent_store.RDS")
     return(tmp |>  dplyr::filter(Timestamp > since))
@@ -81,7 +81,7 @@ update_submissions <- function(home = ".") {
 #' @rdname grading
 #' @export
 document_names <- function(home, since = "2000-1-1 00:00:01 UTC") {
-  since <- convert_time(since)
+  since <- convert_time_helper(since)
   Tmp <- get_historic_data(home, since)
   # # Pull out the submissions
   # for_short <- Tmp[[3]] |> substr(3, 100)  # Has the document ID.
@@ -99,8 +99,50 @@ document_names <- function(home, since = "2000-1-1 00:00:01 UTC") {
     .by = docid)
 }
 
+#' Summarize all of a student's submissions from a single file.
+#'
+summarize_student_doc <- function(
+    Submissions = get_historic_data(),
+    docid = "03-exercises.rmarkdown",
+    student = "dtkaplan@gmail.com",
+    since = "2000-1-1 00:00:01 UTC",
+    until = Sys.Date() + (24*60*60 - 1)) {
+  Submissions <- Submissions |>
+    filter(student == email,
+           docid == docid,
+           since <= Timestamp,
+           until >= Timestamp)
+  convert_to_df <- function(x) jsonlite::parse_json(x, simplifyVector = TRUE)
+  Subs <- lapply(Submissions$contents, FUN = convert_to_df)
+
+  # Grab the MC component
+  MC <- collect_component(Subs, "MC", Submissions$Timestamp)
+  Essays <- collect_component(Subs, "Essays", Submissions$Timestamp)
+  # Handle differently, since each submission$R is a character vector
+  # with the timestamp already embedded
+  R <- c(sapply(Subs, function(x) x$R)) |>
+    parse_webr_event() |> # deparse each R item
+    unique() |> # avoid duplicates
+    arrange(label, time)
+  # Note: the duplicates may arise because webr keeps a cumulative history
+  # of R commands in any one session. If the student submits twice from the same
+  # session.
+
+  list(MC = MC, Essays = Essays, R = R)
+
+  # NEED TO PROCESS MC and Essays to keep just the last non-skipped item submitted.
+  # There should in the end be just one row for each itemid
+}
+
+# NOTES ON PROCESS
+# 1. Get all submissions from a given student from a given assignment: summarize_student_doc()
+#    This uses historic data.
+#    Result: three data frames: MC, essays, webr
+
+
+
 ## Helpers
-convert_time <- function(datetime) {
+convert_time_helper <- function(datetime) {
   if (inherits(datetime, "POSIXlt")) return(datetime)
   if (inherits(datetime, "character"))
     return(as.POSIXlt(datetime, tz = "UTC"))
@@ -108,9 +150,38 @@ convert_time <- function(datetime) {
 }
 
 # Just for raw submissions, as read from the repo
-get_doc_id <- function(submission) {
+get_doc_id_helper <- function(submission) {
   for_short <- submission[[3]] |> substr(3, 100)  # Has the document ID.
   shorter <- gsub("\"", "", for_short)
   # Pull out the document name
   gsub("docid\\:(.*),MC.*$", "\\1", shorter)
 }
+
+# Turn the stuff in the webr history into a simple data frame
+parse_webr_event <- function(events=goo$R) {
+  chunks <- stringr::str_extract(events, "(chunk: [^,]*)")
+  chunks <- gsub("chunk: ", "", chunks)
+  times <- stringr::str_extract(events, "(time: .*), code")
+  times <- gsub(", code", "", gsub("time: ", "", times))
+  code <- stringr::str_extract(events, "\n([^:]*$)")
+  code <- gsub("^\n", "", code)
+  tibble(label = chunks, text = code, time = times)
+
+}
+
+# Construct a data frame from the named component of a submission
+collect_component <- function(submissions, component = "MC", timestamps) {
+  getter <- function(item) {
+    item[[component]]
+  }
+  component <- lapply(submissions, FUN = getter)
+
+  if (!missing(timestamps)) {
+    # Check this just as a reminder
+    if (length(timestamps) != length(component)) stop("Must be a timestamp for every submission")
+    for (k in 1:length(component)) component[[k]]$time <- timestamps[k]
+  }
+
+  dplyr::bind_rows(component)
+}
+
